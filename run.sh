c3c8a6f2029956f7a153db4f142b18049686b483
hdhwang <hadong0624@naver.com>
2024-12-13 12:31:35 +0900

    uwsgi -> gunicorn으로 변경

--- run.sh
+++ run.sh
@@ -2,10 +2,39 @@
 export DJANGO_SETTINGS_MODULE="config.settings.production"
 SCRIPT_PATH=$(dirname $(realpath $0))
 
+LOGS_PATH=${SCRIPT_PATH}/logs
+ACCESS_LOG_FILE=${LOGS_PATH}/gunicorn-access.log
+ERROR_LOG_FILE=${LOGS_PATH}/gunicorn-error.log
+
+RUN_PATH=${SCRIPT_PATH}/run
+SOCKET_FILE=${RUN_PATH}/kms.sock
+PID_FILE=${RUN_PATH}/kms.pid
+WORKERS=3
+THREADS=30
+TIMEOUT=300
+
+if [ ! -d $LOGS_PATH ] ; then
+  mkdir -p $LOGS_PATH
+fi
+
+if [ -e ${SOCKET_FILE} ]; then
+    rm -f ${SOCKET_FILE}
+fi
+
 source $SCRIPT_PATH/venv/bin/activate
 $SCRIPT_PATH/venv/bin/python3 $SCRIPT_PATH/manage.py crontab add
 
 sleep 3
 
-nohup uwsgi --ini $SCRIPT_PATH/config/uwsgi.ini >/dev/null 2>&1 &
+gunicorn config.wsgi:application \
+--bind unix:${SOCKET_FILE} \
+--workers=${WORKERS} \
+--threads=${THREADS} \
+--pid ${PID_FILE} \
+--access-logfile=${ACCESS_LOG_FILE} \
+--error-logfile=${ERROR_LOG_FILE} \
+--timeout {TIMEOUT}
+--preload \
+--daemon \
+
 deactivate
--- stop.sh
+++ stop.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 export DJANGO_SETTINGS_MODULE="config.settings.production"
 SCRIPT_PATH=$(dirname $(realpath $0))
-pid_file=$SCRIPT_PATH"/tmp/kms.pid"
+
+RUN_PATH=${SCRIPT_PATH}/run
+PID_FILE=${RUN_PATH}/kms.pid
 
 source $SCRIPT_PATH/venv/bin/activate
-uwsgi --stop $pid_file
+if [ -f $PID_FILE ]; then
+    # PID 파일이 있으면 종료
+    kill -9 $(cat $PID_FILE)
+    rm -f ${PID_FILE}
+    echo "kms stopped successfully."
+fi
 $SCRIPT_PATH/venv/bin/python3 $SCRIPT_PATH/manage.py crontab remove
 deactivate
